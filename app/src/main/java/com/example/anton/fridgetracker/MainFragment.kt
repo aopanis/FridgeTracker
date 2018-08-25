243f392c103cf8b3280aa89d944589729a5f8562
Anton Panis <aopanis@gmail.com>
2018-08-25 14:09:13 -0700

    fragment

---
+++ app/src/main/java/com/example/anton/fridgetracker/MainFragment.kt
@@ -0,0 +1,158 @@
+package com.example.anton.fridgetracker
+
+import android.content.Context
+import android.content.Intent
+import android.os.AsyncTask
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.Button
+
+import kotlinx.android.synthetic.main.fragment_main.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [MainFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class MainFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+//    private var listener: OnFragmentInteractionListener? = null
+
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+
+
+    private var mDb: FoodItemDatabase? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        val currentContext = context
+
+        if(currentContext != null) {
+            mDb = FoodItemDatabase.getInstance(currentContext)
+        }
+
+        val dataQuery = DatabaseQueryTask()
+
+        dataQuery.execute()
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        floatingActionButton.setOnClickListener {_ ->
+            val intent = Intent(context, AddItem::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+
+    private inner class DatabaseQueryTask: AsyncTask<Void, Void, Array<FoodItem>?>() {
+        override fun doInBackground(vararg p0: Void?): Array<FoodItem>? {
+            val myDataset = mDb?.foodItemDao()?.getAll()?.toTypedArray()
+            return myDataset
+        }
+
+        override fun onPostExecute(result: Array<FoodItem>?) {
+            super.onPostExecute(result)
+            viewManager = LinearLayoutManager(context)
+            if(result != null) {
+                viewAdapter = FoodItemAdapter(result)
+            }
+
+            recyclerView.apply {
+                setHasFixedSize(true)
+
+                layoutManager = viewManager
+
+                adapter = viewAdapter
+            }
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+
+    // TODO: Rename method, update argument and hook method into UI event
+//    fun onButtonPressed(uri: Uri) {
+//        listener?.onFragmentInteraction(uri)
+//    }
+
+//    override fun onAttach(context: Context) {
+//        super.onAttach(context)
+//        if (context is OnFragmentInteractionListener) {
+//            listener = context
+//        } else {
+//            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+//        }
+//    }
+
+    override fun onDetach() {
+        super.onDetach()
+//        listener = null
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+/*    interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction(uri: Uri)
+    }*/
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment MainFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+                MainFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
+                }
+    }
+}
