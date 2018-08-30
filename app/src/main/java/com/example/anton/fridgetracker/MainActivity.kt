166056becce381bf09f4b1601621472fc4851ab5
Anton Panis <aopanis@gmail.com>
2018-08-29 21:39:08 -0700

    fragment

--- app/src/main/java/com/example/anton/fridgetracker/MainActivity.kt
+++ app/src/main/java/com/example/anton/fridgetracker/MainActivity.kt
@@ -1,64 +1,53 @@
 package com.example.anton.fridgetracker
 
 import android.content.Intent
-import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.support.v7.widget.LinearLayoutCompat
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.view.View
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBar
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
-    private lateinit var viewManager: RecyclerView.LayoutManager
-
-    private var mDb: FoodItemDatabase? = null
-
-
+    private lateinit var mDrawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mDb = FoodItemDatabase.getInstance(this)
-
-        val dataQuery = DatabaseQueryTask()
-
-        dataQuery.execute()
-
-//        viewAdapter = FoodItemAdapter(arrayOf("Hello", "this", "is"))
-
-    }
-
-    fun addItem(view: View) {
-        val intent = Intent(this, AddItem::class.java)
-        startActivity(intent)
-    }
-
-    private inner class DatabaseQueryTask: AsyncTask<Void, Void, Array<FoodItem>?>() {
-        override fun doInBackground(vararg p0: Void?): Array<FoodItem>? {
-            val myDataset = mDb?.foodItemDao()?.getAll()?.toTypedArray()
-            return myDataset
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu)
         }
 
-        override fun onPostExecute(result: Array<FoodItem>?) {
-            super.onPostExecute(result)
-            viewManager = LinearLayoutManager(this@MainActivity)
-            if(result != null) {
-                viewAdapter = FoodItemAdapter(result)
-            }
+        mDrawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
 
-            recyclerView.apply {
-                setHasFixedSize(true)
+        nav_view.setNavigationItemSelectedListener { menuItem ->
 
-                layoutManager = viewManager
+            menuItem.isChecked = true
 
-                adapter = viewAdapter
-            }
+            mDrawerLayout.closeDrawers()
+
+            true
+
         }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                mDrawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
